a185b12b73e45d201197fc8e1d88c858d912f1cf
mdijkstra <mdijkstra@work>
2013-10-01 11:11:40 +0200

    Added two part model

--- analysis/functions.R
+++ analysis/functions.R
@@ -21,6 +21,47 @@ correlation = function(p, voc){
 	list(cor.matrix.cor = cor.matrix.cor, cor.matrix.p = cor.matrix.p)
 }
 
+correlation.two.part = function(p, voc){
+	n.phenotypes = ncol(p)
+	cor.matrix.cor	= NULL # [phenotype.i, voc.j] correlation
+	cor.matrix.p	= NULL # [phenotype.i, voc.j] p-value
+
+	# Determine which vocs are zero
+	voc.is.zero = voc
+	voc.is.zero = apply(t(voc), 1, function(x) 0 == x | is.na(x))
+
+	cor.test.PiVOCj = function(VOCj, Pi) cor.test(VOCj, Pi, use = "complete.obs", method = "spearman")
+	wilcox.test.PiVOCj = function(zero, Pi) {
+		if (0 == length(which(zero)) | 0 == length(which(!zero)) | all(is.na(Pi[zero])) | all(is.na(Pi[!zero]))) {
+			return(1)
+		} else {
+			pw = wilcox.test(Pi[zero], Pi[!zero])$p.value
+			if (is.finite(pw)) return(pw) else return(1)
+		}
+	}
+	
+	for (i in 1:n.phenotypes)
+	{
+		#print(str_c("Phenotype ", i))
+		this.cor.list		= apply(voc, 2, cor.test.PiVOCj, p[, i])
+		cor.values			= sapply(this.cor.list, function(v) v$estimate)
+		spearman.p.values	= sapply(this.cor.list, function(v) v$p.value)
+		
+		wilcox.p.values	= apply(voc.is.zero, 2, wilcox.test.PiVOCj, p[, i])
+		
+		
+		cor.matrix.cor	= rbind(cor.matrix.cor, cor.values)
+		cor.matrix.p	= rbind(cor.matrix.p, spearman.p.values * wilcox.p.values)
+
+		print(str_c(round(i / n.phenotypes * 100), "% completed..."))
+	}
+	
+	rownames(cor.matrix.cor) = rownames(cor.matrix.p) = colnames(p)
+	colnames(cor.matrix.cor) = colnames(cor.matrix.p) = colnames(voc)
+	
+	list(cor.matrix.cor = cor.matrix.cor, cor.matrix.p = cor.matrix.p)
+}
+
 fdr.quick = function(p.values.real, p.values.random, n.elements = 2e3)
 {
 	a	= sort(as.vector(p.values.real[which(!is.na(p.values.real))]))
