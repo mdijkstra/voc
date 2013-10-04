446d8d263bfe78a19b2f5ae09463536d6cacf72f
mdijkstra <mdijkstra@work>
2013-10-04 15:07:05 +0200

    Correlate VOC phenotype with improved FDR, for all/male/female

---
+++ analysis/correlateVOCPhenotype_ImprovedFDR.R
@@ -0,0 +1,148 @@
+#
+## Initialize
+#
+rm(list=ls(all=F))
+library(stringr); library(pheatmap)
+setwd('/Users/mdijkstra/Dropbox/Documents/collaboration/lude/voc/analysis')
+source('functions.R') # source functions
+
+# Define unique stamp which we put in file name
+non.zero.fraction = .5 # at least
+select.gender = "Male" #Male, Female, All
+ignore.zeros = T
+n.random.iterations = 10
+unique.stamp = str_c(select.gender, "_", "Nonzerofraction", non.zero.fraction, if (ignore.zeros) "_IgnoreZeros" else "", "_", n.random.iterations, "RandomIterations_", str_replace_all(format(Sys.time(), "%X"), ":", ""))
+
+#
+## Load data
+#
+source('load_data.R') # p, voc
+
+#
+## Preprocessing
+#
+
+# Gender selection:
+index.male		= which(1 == p[, "GESLACHT"])
+index.female	= which(2 == p[, "GESLACHT"])
+
+if ("Male" == select.gender)	individuals.keep = index.male
+if ("Female" == select.gender)	individuals.keep = index.female
+if ("All" == select.gender)		individuals.keep = 1:nrow(p)
+
+p	= p[individuals.keep, ]
+voc	= voc[individuals.keep, ]
+
+# Remove columns with >20% 0's. Also try >50%
+index.keep = apply(voc, 2, function(vec) non.zero.fraction <= (1 - length(which(0 == vec))/length(vec)))
+voc = voc[, index.keep]
+if (ignore.zeros) voc[which(0 == voc)] = NA
+
+# Constants
+n.individuals	= nrow(p)
+n.phenotypes	= ncol(p)
+n.vocs			= ncol(voc)
+
+#
+## Calculate correlations
+#
+
+# Calculate cor/p-values for _real_ data
+real = correlation.two.part(p, voc)
+
+# Extract only relevant p-values (i.e. Pheno x VOC)
+p.values.real = real$cor.matrix.p
+
+# Determine FDR
+print("============== Determine FDR =================")
+p.values.random = NULL
+for (i in 1:n.random.iterations)
+{
+	print(str_c("~-~-~-~-~- Iteration ", i, " of ", n.random.iterations, " ~-~-~-~-~-"))
+	
+	# Randomize pvoc matrix
+	p.randomized = p[sample(1:n.individuals), ]
+
+	# Calculate cor/p-values for _random_ data
+	random = correlation.two.part(p.randomized, voc)
+
+	# Get p-values
+	p.values.random = c(p.values.random, as.vector(random$cor.matrix.p))
+}
+
+##################################
+#
+## Which p-values are significant after Bonferroni correction?
+## Which have FDR < 0.05?
+#
+fdr = fdr.manyIterations(p.values.real, p.values.random, n.random.iterations, n.elements = 2e3)
+
+# Select p-values for which FDR < .05
+index.fdr = max(which(fdr$fdr <= .05))
+hits = is.finite(index.fdr)
+p.values.significant = if (!hits) NULL else fdr$p[1:index.fdr] #which(fdr$fdr <= .05)]
+
+if (T) {
+	pdf(str_c('plots/', 'fdr_', unique.stamp, '.pdf'))
+		n.elements.zoom = min(1e2, length(fdr$p))
+		plot(log(fdr$p[1:n.elements.zoom], 10), fdr$fdr[1:n.elements.zoom], t='b', pch=19, lwd=2, axes=F, xlab="log( p-value )", ylab="FDR", col="gray", main = str_c("#signif = ", if (hits) index.fdr else 0))
+		axis(1); axis(2, las=2)
+		alpha.bonf = 0.05 / n.phenotypes / n.vocs
+		abline(v = log(alpha.bonf, 10), lty=2, lwd = 2) # Bonferroni
+		abline(h = 0.05, lty=2, lwd = 2) # FDR
+		
+		if (hits)
+		{
+			par(new = T)
+			plot(log(fdr$p[1:n.elements.zoom], 10), fdr$fdr[1:n.elements.zoom], t = 'p', pch=c(rep(1, index.fdr), rep(NA, n.elements.zoom - index.fdr)), col="red", axes=F, xlab="", ylab="", lwd = 2)	
+		}
+	dev.off()
+}
+
+# Find indices of these p-values
+summary = NULL
+for (p.value in p.values.significant) {
+	index = which(p.value == real$cor.matrix.p, arr.in = T)
+	if (1 == nrow(index)) {
+		summary = rbind(summary, c(pheno = rownames(index), voc = colnames(voc)[index[,2]], p.value = p.value))
+	}
+}
+
+summary.pheno	= unique(summary[, "pheno"])
+summary.voc		= unique(summary[, "voc"])
+summary.matrix	= matrix(NA, nrow = length(summary.pheno), ncol = length(summary.voc), dimnames = list(summary.pheno, summary.voc))
+if (hits) for (i in 1:nrow(summary)) {
+	summary.matrix[summary[i, "pheno"], summary[i, "voc"]] = summary[i, "p.value"]
+}
+summary.matrix = apply(summary.matrix, 1, as.numeric)
+if (is.numeric(summary.matrix)) {
+	summary.matrix = as.matrix(summary.matrix, nrow = 1, ncol = 1)
+	colnames(summary.matrix) = summary.pheno
+}
+rownames(summary.matrix) = summary.voc
+
+
+if (hits & 1 < prod(dim(summary.matrix))) pheatmap(log(t(summary.matrix), 10), cluster_row = F, cluster_col = F, filename = str_c("plots/", "phenoVsVocsPvalueOnLog10Scale_", unique.stamp, ".pdf"))
+
+
+# Also show p-value distribution:
+if (F)
+{
+	pdf('plots/random_p-value_distribution_two-part.pdf')
+		hist(p.values.random, axes = F, xlab = "p-values", main = "Histogram of 'random' p-values two-part model")
+		axis(1)
+		axis(2, las = 2)
+	dev.off()
+	pdf('plots/random_p-value_distribution_spearman.pdf')
+		hist(as.vector(random$cor.matrix.p), axes = F, xlab = "p-values", main = "Histogram of 'random' p-values, spearman")
+		axis(1)
+		axis(2, las = 2)
+	dev.off()
+}
+
+# Save image with R-data enabling a reuse of 'random iterations'
+save.image(str_c("/Volumes/lldeep/rimages/", unique.stamp, ".RData"))
+
+
+
+#
